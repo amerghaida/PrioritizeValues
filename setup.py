1894b609ff4984837e6e04d05f08df6045910240
William Rusnack <williamrusnack@gmail.com>
2016-11-30 16:32:42 -0500

    Added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='PrioritizeValues',
+      version='0.0.0',
+      license='MIT',
+      description='Prioritizes or weights values for comparisons. Useful for grading functions where one value is more important than another.',
+      author='William Rusnack',
+      author_email='williamrusnack@gmail.com',
+      url='https://github.com/BebeSparkelSparkel/PrioritizeValues',
+      classifiers=['Development Status :: 2 - Pre-Alpha', 'Programming Language :: Python :: 3'],
+      py_modules=["PrioritizeValues"],
+     )
